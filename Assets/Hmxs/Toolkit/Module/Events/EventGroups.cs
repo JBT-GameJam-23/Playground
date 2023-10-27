123bc33547234ccd44ec6fa4173998a1dfe2e9ed
Hmxs <18321938171@163.com>
2023-10-28 00:24:28 +0800

    complete input module - 202310280024

--- Assets/Hmxs/Toolkit/Module/Events/EventGroups.cs
+++ Assets/Hmxs/Toolkit/Module/Events/EventGroups.cs
@@ -7,24 +7,11 @@
     {
         public static class General
         {
-            public const string TestNoParamEvent = "TestNoParamEvent";
-            public const string Test3ParamEvent = "Test3ParamEvent";
         }
-        
-        public static class Quests
-        {
-            public const string QuestStart = "QuestStart"; // 任务开始时
-            public const string QuestAdvance = "QuestAdvance"; // 任务推进时
-            public const string QuestFinish = "QuestFinish"; // 任务完成时
-            public const string QuestStateChange = "QuestStateChange"; // 任务状态变化时
-            public const string QuestStepDataUpdate = "QuestStepDataUpdate"; // StepData持久化
 
-            public const string TestQuestOnCoinCollected = "TestQuestOnCoinCollected";
-        }
-        
         public static class Player
         {
-            public const string SubmitPressed = "SubmitPressed";
+            public const string IsSucking = "IsSucking";
         }
     }
 }
